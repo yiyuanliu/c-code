06e24989c4c3241d9bf1884b1a22652094a62f19
Yiyuan Liu <yiyuanliu1997@gmail.com>
2022-01-02 13:59:45 +0800

    fix mlock test
    ```
    128B            3.15us
    256B            1.85us
    512B            1.94us
    1KB             2.1us
    2KB             2.29us
    4KB             2.71us
    8KB             3.34us
    16KB            4.71us
    32KB            7.44us
    64KB            12.3us
    128KB           22.5us
    256KB           43us
    512KB           81.4us
    1MB             159us
    2MB             322us
    4MB             619us
    8MB             1.22ms
    16MB            2.46ms
    32MB            15.3ms
    
    zero memory before lock
    128B            3.6us
    256B            1.85us
    512B            1.89us
    1KB             2.03us
    2KB             2.15us
    4KB             2.52us
    8KB             3.02us
    16KB            4.13us
    32KB            6.2us
    64KB            12.6us
    128KB           22.9us
    256KB           43us
    512KB           81.6us
    1MB             159us
    2MB             312us
    4MB             615us
    8MB             1.24ms
    16MB            2.45ms
    32MB            8.03ms
    ```

--- benchmark/mlock.cc
+++ benchmark/mlock.cc
@@ -1,5 +1,6 @@
 #include "tools.h"
 
+#include <cerrno>
 #include <cstdlib>
 #include <cstring>
 #include <iostream>
@@ -11,13 +12,23 @@ static struct ibv_pd *ib_pd = NULL;
 static bool odp = false;
 
 static constexpr size_t kLoop = 1000;
+static constexpr size_t kMaxSize = 32 * 1024ULL * 1024ULL;
 
 static void benchmark(char *ptr, size_t len, int loop) {
   uint64_t total = 0;
   for (int i = 0; i < loop; i++) {
     auto begin = clock_ns();
-    mlock(ptr, len);
-    munlock(ptr, len);
+    int ret = mlock(ptr, len);
+    if (ret != 0) {
+      std::cerr << "Failed to mlock, ret " << errno << ", check ulimit please."
+                << std::endl;
+      abort();
+    }
+    ret = munlock(ptr, len);
+    if (ret != 0) {
+      std::cerr << "Failed to munlock, ret " << errno << std::endl;
+      abort();
+    }
     ptr += len;
     auto end = clock_ns();
     total += end - begin;
@@ -28,7 +39,17 @@ static void benchmark(char *ptr, size_t len, int loop) {
 }
 
 int main() {
-  for (size_t mem = 128; mem <= 16 * 1024ULL * 1024ULL; mem *= 2) {
+  for (size_t mem = 128; mem <= kMaxSize; mem *= 2) {
+    char *ptr = (char *)malloc(mem * kLoop);
+    if (ptr == nullptr)
+      break;
+    benchmark(ptr, mem, kLoop);
+    free(ptr);
+  }
+  std::cout << std::endl;
+
+  std::cout << "zero memory before lock" << std::endl;
+  for (size_t mem = 128; mem <= kMaxSize; mem *= 2) {
     char *ptr = (char *)malloc(mem * kLoop);
     if (ptr == nullptr)
       break;
